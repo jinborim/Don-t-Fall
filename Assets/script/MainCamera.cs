5284533ed3e2afc9e16a35bc8854974eedba4c7a
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-07 14:56:19 +0900

    Update and rename Assets/MainCamera.cs to Assets/script/MainCamera.cs

--- Assets/script/MainCamera.cs
+++ Assets/script/MainCamera.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    public Transform target; //따라갈 대상
-    public Vector3 offset; //Ball과 카메라 사이의 거리
+    public Transform target; 
+    public Vector3 offset; 
 
     void Start()
     {
-        //시작할 때 공과 카메라 사이의 실제 거리를 계산해서 저장
         if(target != null)
         {
             offset = transform.position - target.position;
@@ -18,11 +17,9 @@ public class MainCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        //LateUpdate는 카메라에 사용하기 제일 좋음
         if(target != null)
         {
             transform.position = target.position + offset;
-            //공의 현재 위치에 처음에 계산한 거리를 더해서 카메라 위치를 둠
         }
     }
 }
